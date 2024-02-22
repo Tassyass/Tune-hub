bfe5bdf3b04878b199dbcb9f2e33bfee97e8bcde
AbaadirYasin <https://github.com/AbaadirYasin>
2024-02-22 15:11:55 +0300

    first

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ["./src/**/*.{js,jsx,ts,tsx}",];
+export const theme = {
+  extend: {},
+  container: {
+    center: true,
+  },
+};
+export const plugins = [];
+
