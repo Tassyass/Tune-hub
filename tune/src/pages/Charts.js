7703e21963b5f14ac8f6c9f73da15f09d5565d59
AbaadirYasin <https://github.com/AbaadirYasin>
2024-02-22 15:10:48 +0300

    first

---
+++ tune/src/pages/Charts.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+import TopSongsComponent from '../components/TopSongsComponent'
+import Footer from '../components/Footer'
+import Logo from '../components/Logo'
+
+function Charts() {
+  return (
+    <div>
+        <TopSongsComponent/>
+    </div>
+  )
+}
+
+export default Charts
---
+++ tune/src/pages/Play.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Player from '../components/Player'
+
+function Play() {
+  return (
+    <div className='content-center'>
+        <Player/>
+    </div>
+  )
+}
+
+export default Play
